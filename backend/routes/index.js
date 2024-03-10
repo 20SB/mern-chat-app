4625ef01634a4dea15b7e366f4b7e54b67f7e5f9
20SB <subhabiswal20@gmail.com>
2024-03-10 21:31:08 +0530

    node home error removed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    res.send(`<h1>Hiii, You are on ChitChat Server</h1><p>${url}<p>`);
+    res.send(`<h1>Hiii, You are on ChitChat Server</h1>`);
 });
 router.use("/api", require("./apiRoutes"));
 router.use("/auth", require("./googleOauth"));
