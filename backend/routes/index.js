8a8e77d49963f1590179f05b02fef3a33c780424
SB-wstf <subha.biswal@thewasserstoff.com>
2025-03-05 18:49:58 +0530

    check env

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,18 @@
 // backend\routes\index.js
 const express = require("express");
+const env = require("../config/environment");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
     res.send(`<h1>Hiii, You are on ChitChat Server</h1>`);
+});
+router.get("/envData", async (req, res) => {
+    const envData = env
+    return res.status(200).json({
+        success: true,
+        envData: envData,
+        users,
+    });
 });
 router.use("/api", require("./apiRoutes"));
 router.use("/auth", require("./googleOauth"));
